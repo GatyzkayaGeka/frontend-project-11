8ca01d2a26b499446bf000f58a3924ffe26d8f91
GatyzkayaGeka <gatyzkayageka@gmail.com>
2023-07-21 14:23:21 +0500

    edits1 try 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
+export default {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
